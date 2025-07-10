f9b47b25c5bd9e8801c0615e1450509ea34d90ad
Lukas Østergaard <lukas.ostergaard@abacusmedicine.com>
2025-07-10 14:54:36 +0200

    secrect message for birthday added

--- cv_web-page/src/components/AboutMe.tsx
+++ cv_web-page/src/components/AboutMe.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 
 const AboutMe = () => {
+  let dateTime = new Date();
+  const birthday = new Date(1999, 2, 15);
+
+  function getAge(date: Date) {
+    const birthYear = birthday.getFullYear();
+    let dateYear = date.getFullYear();
+
+    if (date.getMonth() <= 2) {
+      if (date.getDay() < 15) {
+        return dateYear - birthYear - 1;
+      }
+    } else {
+      return dateYear - birthYear;
+    }
+  }
+
+  const [bDayMessage, setbDayMessage] = useState("");
+  useEffect(() => {
+    if (
+      dateTime.getDate() === birthday.getDate() &&
+      dateTime.getMonth() === birthday.getMonth()
+    ) {
+      setbDayMessage(". Today is my birthday! 🥳");
+      document.title = "It's my birthday!";
+    }
+  }, [dateTime]);
+
   return (
     <>
       <div id="about-me">
@@ -9,29 +36,48 @@ const AboutMe = () => {
           <h1>About me</h1>
           <br />
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            vitae dignissimos sit voluptatum id nobis fugit natus. Laudantium
-            commodi unde adipisci incidunt dolores cumque ratione nemo sunt?
-            Aliquam, at minus?
+            I enjoy working with my different interests. Both in a team with
+            peers to bounce ideas off on, but also independently. I enjoy
+            socializing with friends both in-person, but also over a good game
+            on the computer.
           </p>
         </div>
         <div className="double-element" style={{ border: "1px solid red" }}>
-          <div style={{ border: "1px solid red", margin: "0.5rem" }}>
-            <h2>personal information</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-              minus natus, veniam doloribus dolorum neque repellat, aliquid
-              molestiae ut aut itaque, mollitia eum! Quas rerum eligendi,
-              voluptas quo nihil ex?
-            </p>
+          <div
+            style={{
+              height: "12rem",
+              width: "25rem",
+              border: "1px solid red",
+              margin: "0.5rem",
+            }}
+          >
+            <h2>Personal information</h2>
+            <ul className="emoji-list">
+              <li className="item-1">
+                Age: {getAge(dateTime)} {bDayMessage}
+              </li>
+              <li className="item-2">Email: lukasglo12@gmail.com</li>
+              <li className="item-3">Phone: +45 22 55 08 35</li>
+              <li className="item-4">
+                Address: Ved Mønten 10, 2300, København S
+              </li>
+              <li className="item-5">Nationality: Danish</li>
+            </ul>
           </div>
-          <div style={{ border: "1px solid red", margin: "0.5rem" }}>
-            <h2>interests</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
-              illum, temporibus iure repudiandae atque voluptate, facere nulla
-              itaque quam, neque molestiae possimus! Nostrum error cum expedita
-              a sapiente? Sunt, repudiandae.
+          <div
+            style={{
+              height: "12rem",
+              width: "25rem",
+              border: "1px solid red",
+              margin: "0.5rem",
+            }}
+          >
+            <h2 className="h2">Interests</h2>
+            <p className="p">
+              I enjoy keeping up with the newest technologies, movies, and
+              games. Watching and playing football in my spare time, and animals
+              have a special place in my heart. Along with collecting and
+              designing LEGO.
             </p>
           </div>
         </div>
